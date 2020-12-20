624bc7648429e0591d0d131a1d187d04f4c67c85
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-12-20 11:39:42 -0300

    added POST route

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,5 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento'))
+
+    app.post('/atendimentos', (req, res) => {res.send('rota de atendimento POST')})
 }
