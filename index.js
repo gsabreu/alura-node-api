1c8e4099f41a36abb3ef78b987eb0c07839095e2
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-12-20 12:28:35 -0300

    added mysql connection

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const customExpress = require('./config/customExpress')
+const connection= require('./infra/connection')
 
-const app = customExpress()
+connection.connect(erro =>{
+    if(erro) {
+        console.log(erro);
+    } else {
+        console.log('database connection was success');
 
-app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+        const app = customExpress()
+        app.listen(3000, () => console.log('servidor rodando na porta 3000'))
+    }
+})
