59125c8be1f4958ffa68c7e50b4792acdb0651d4
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-12-20 14:08:55 -0300

    Adding validations

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -5,6 +5,31 @@ class Atendimento {
     add(atendimento, res){
         const createdDate = moment().format('YYYY-MM-DD HH:MM:SS')
         const date = moment(atendimento.date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+
+        const isValidDate = moment(date).isSameOrAfter(createdDate)
+        const isValidClient = atendimento.client.length >= 5
+
+        const validations = [
+            {
+                name: 'date',
+                valid : isValidDate,
+                message: 'Date needs to be bigger than actual date'
+            },
+            {
+                name: 'client',
+                valid : isValidClient,
+                message: 'Client needs to be bigger than 5 chars'
+            }
+        ]
+
+        const errors = validations.filter(data => !data.valid)
+        const hasErrors = errors.length
+
+        if(hasErrors){
+            res.status(400).json(errors)
+        }
+        else {
+
         const atendimentoDatado = {...atendimento, createdDate, date}
 
         const sql = 'INSERT INTO atendimentos SET ?'
@@ -18,6 +43,7 @@ class Atendimento {
             }
         })
 
+        }
     }
 }
 
