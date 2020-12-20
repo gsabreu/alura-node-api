321125cd9bed4c438b4bdaf4c12211b119062729
Guilherme Abreu <sabreuguilherme@gmail.com>
2020-12-20 11:44:42 -0300

    ready to json

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,7 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimento'))
 
-    app.post('/atendimentos', (req, res) => {res.send('rota de atendimento POST')})
+    app.post('/atendimentos', (req, res) => {
+        console.log(req.body);
+        res.send('rota de atendimento POST')})
 }
