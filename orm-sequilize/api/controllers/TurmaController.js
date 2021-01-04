8aebe381668545986f38b670dce48195e3486525
Guilherme Abreu <sabreuguilherme@gmail.com>
2021-01-04 20:01:59 -0300

    added operators to search by date

--- orm-sequilize/api/controllers/TurmaController.js
+++ orm-sequilize/api/controllers/TurmaController.js
@@ -1,9 +1,16 @@
 const database = require('../models')
+const Sequelize = require('sequelize')
+const Op = Sequelize.Op
 
 class TurmaController {
     static async getAll(req, res){
+        const {data_inicial, data_final } = req.query
+        const where = {}
+        data_inicial || data_final ? where.data_inicial = {} : null
+        data_inicial ? where.data_inicio[Op.gte] = data_inicial : null
+        data_final ? where.data_inicio[Op.lte] = data_final: null
         try {
-            const turmas = await database.Turmas.findAll()
+            const turmas = await database.Turmas.findAll({ where })
             return res.status(200).json(turmas)
         } catch (error) {
             return res.status(500).json(error.message)
